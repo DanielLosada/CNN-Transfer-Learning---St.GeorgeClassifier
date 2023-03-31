24098a6256df5e246bd0dd421ccbae4e5f769255
DanielLosada <daniellosad@gmail.com>
2023-03-31 20:02:21 +0200

    first functional model created

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # St.GeorgeClassifier
+
+## Installation
+### With Conda
+Create a conda environment by running
+```
+conda create --name StGeorgeClassifier python=3.8
+```
+Then, activate the environment
+```
+conda activate StGeorgeClassifier
+```
+and install the dependencies
+```
+pip install -r requirements.txt
+```
+
+
+## Running the project
+
+To run the project without hyperparameter tuning, run
+```
+python main.py
+```
---
+++ model.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+from convblock import ConvBlock
+from linearblock import LinearBlock
+
+class MyModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = ConvBlock(num_inp_channels=3, num_out_fmaps=6, kernel_size=5)
+        self.conv2 = ConvBlock(num_inp_channels=6, num_out_fmaps=6, kernel_size=5)
+        self.conv3 = ConvBlock(num_inp_channels=6, num_out_fmaps=6, kernel_size=5)
+
+        self.mlp = nn.Sequential(
+            LinearBlock(1944, 84),
+            nn.Linear(84, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        x = x.view(x.size(0), -1)  # flatten the output for the MLP
+        x = self.mlp(x)
+        return x
